9464570edd228d6b85a471f8a717fdd4a78e59ec
Merlijn Busch <merlijn1busch@gmail.com>
2021-09-14 14:17:56 +0200

    added test cases for database functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,12 @@
 package com.company;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.bson.Document;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.company.test.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+         System.out.println("Running program!");
 
-        //https://www.mongodb.com/developer/quickstart/java-setup-crud-operations/
-        String conS = "mongodb+srv://new-user31:qx7AA7LwaCD5WHj@cluster0.twb3h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-        try (MongoClient mongoClient = MongoClients.create(conS)) {
-            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
-            databases.forEach(db -> System.out.println(db.toJson()));
-        }
+         Connection test = new Connection();
     }
 }
