8e5eafcf8887329f446fe7a097202663b18b8f64
Merlijn Busch <merlijn1busch@gmail.com>
2021-10-05 12:32:31 +0200

    updated migration

--- src/main/java/app/database/migrations/Migrator.java
+++ src/main/java/app/database/migrations/Migrator.java
@@ -6,6 +6,7 @@ import app.database.migrations.partials.GenerateTicketMigration;
 import app.database.migrations.partials.GenerateUserMigration;
 
 import java.text.ParseException;
+import java.util.Random;
 
 public class Migrator {
 
@@ -21,5 +22,20 @@ public class Migrator {
         new GenerateLocationsMigration();
         new GenerateUserMigration();
         new GenerateTicketMigration();
+    }
+
+    protected String generateRandomString(int length){
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+    protected String selectRandomFromArray(String[] array){
+        return array[new Random().nextInt(array.length)];
     }
 }
--- src/main/java/app/database/migrations/partials/GenerateTicketMigration.java
+++ src/main/java/app/database/migrations/partials/GenerateTicketMigration.java
@@ -2,55 +2,46 @@ package app.database.migrations.partials;
 
 import app.database.migrations.Migrator;
 import app.helpers.dateParser;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GenerateTicketMigration extends Migrator {
 
     private final String collectionName = "Tickets";
+    private final dateParser parser = new dateParser();
+    private final Date date = new Date(System.currentTimeMillis());
 
     public GenerateTicketMigration() throws ParseException {
-        Date date = new Date(System.currentTimeMillis());
-        dateParser parser = new dateParser();
-
-        new dateParser();
-        Document document1 = new Document("Reported", parser.toString(date))
-                .append("incident", "Flammable laptop.")
-                .append("type", "Hardware")
-                .append("user_id", "obj(id:123123213)")
-                .append("priority", "High")
-                .append("deadline", parser.toString(date))
-                .append("description", "Laptop went up in flames.")
-                .append("status", "escalate");
-
-        Document document2 = new Document("Reported", parser.toString(date))
-                .append("incident", "Software problem.")
-                .append("type", "Software")
-                .append("user_id", "obj(id:123123213)")
-                .append("priority", "High")
-                .append("deadline", parser.toString(date))
-                .append("description", "System 32 was erased.")
-                .append("status", "closed");;
-
-        Document document3 = new Document("Reported", parser.toString(date))
-                .append("incident", "Sandwich stolen.")
-                .append("type", "Service")
-                .append("user_id", "obj(id:123123213)")
-                .append("priority", "High")
-                .append("deadline", parser.toString(date))
-                .append("description", "Ross's turkey sandwich was stolen")
-                .append("status", "normal");;
 
         List<Document> tickets = new ArrayList<>();
-        tickets.add(document1);
-        tickets.add(document2);
-        tickets.add(document3);
+        for(int i = 1; i < 100; i++){
+            tickets.add(generateDocument());
+        }
 
         this.database.insertMany(tickets, this.collectionName);
     }
+
+    private Document generateDocument(){
+        String[] priority = {"High", "Low", "Normal"};
+        String[] type = {"Service", "Hardware", "Software"};
+        String[] status = {"Closed", "Normal", "Escalated"};
+
+        return new Document("Reported", parser.toString(date))
+                .append("incident", generateRandomString( ThreadLocalRandom.current().nextInt(15, 30)))
+                .append("type", selectRandomFromArray(type))
+                .append("user_id", database.findOne(Filters.eq("type", "Service_desk"), "users").get("_id").toString())
+                .append("employee_id", database.findOne(Filters.eq("type", "Employee"), "users").get("_id").toString())
+                .append("priority", selectRandomFromArray(priority))
+                .append("deadline", parser.toString(date))
+                .append("description", generateRandomString( ThreadLocalRandom.current().nextInt(60, 200)))
+                .append("status", selectRandomFromArray(status));
+    }
+
+
 }
