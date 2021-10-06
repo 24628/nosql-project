4357b2961701b7afd560dc234c27cb9b8fc0c406
Merlijn Busch <merlijn1busch@gmail.com>
2021-10-06 23:16:23 +0200

    added circle thingie

--- src/main/java/app/MainApplication.java
+++ src/main/java/app/MainApplication.java
@@ -1,7 +1,10 @@
 package app;
 
 import app.database.migrations.Migrator;
+import app.helpers.Session;
+import app.views.partial.DashboardView;
 import app.views.windows.Form_Login;
+import app.views.windows.MainWindow;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -15,12 +18,15 @@ public class MainApplication extends Application {
 
         // We should open a login window, but that is outside of scope for this lesson.
         // So we go straight to the main window of the application
-
+//
 //        new Migrator();
 //        Migrator.migrationsToRun();
 
-        Form_Login form = new Form_Login();
-        form.getStage().show();
+//        Form_Login form = new Form_Login();
+//        form.getStage().show();
+
+        MainWindow mw = new MainWindow();
+        mw.getStage().show();
 
     }
 
--- src/main/java/app/views/partial/DashboardView.java
+++ src/main/java/app/views/partial/DashboardView.java
@@ -5,14 +5,20 @@ import app.views.windows.MainWindow;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
 import javafx.scene.text.Font;
 
 public class DashboardView extends BaseListView {
 
     private MainWindow main;
+
+    public int pastDeadline = 5;
+    public int totalTickets = 15;
+    public int openTickets = 7;
 
     public DashboardView(MainWindow main){
         this.main = main;
@@ -24,15 +30,42 @@ public class DashboardView extends BaseListView {
 
         // header of dashboard
         HBox dashboard_Header = new HBox();
-        Button show_List = new Button("SHOW LIST");
         Label dashboard_Title = new Label("Current incidents");
 
         dashboard_Header.setSpacing(325);
         dashboard_Header.setAlignment(Pos.CENTER);
         dashboard_Title.setFont(Font.font("Verdana", 30));
-        show_List.setMinWidth(150);
-        show_List.setOnAction(actionEvent -> main.setTableView("Ticket"));
-        dashboard_Header.getChildren().addAll(dashboard_Title, show_List);
+
+        Arc ProgressBarProgress = new Arc();
+        ProgressBarProgress.setRadiusX(150.0f);
+        ProgressBarProgress.setRadiusY(150.0f);
+        ProgressBarProgress.setStartAngle(0);
+        ProgressBarProgress.setLength(10);
+        ProgressBarProgress.fillProperty();
+        ProgressBarProgress.setFill(Color.RED);
+        ProgressBarProgress.setType(ArcType.ROUND);
+
+        Arc ProgressBarHolder = new Arc();
+        ProgressBarHolder.setRadiusX(150.0f);
+        ProgressBarHolder.setRadiusY(150.0f);
+        ProgressBarHolder.setStartAngle(0);
+        ProgressBarHolder.setLength(360.0f);
+        ProgressBarHolder.setFill(Color.GRAY);
+        ProgressBarHolder.setType(ArcType.ROUND);
+
+        Arc InnerCircle = new Arc();
+        InnerCircle.setRadiusX(100.0f);
+        InnerCircle.setRadiusY(100.0f);
+        InnerCircle.setStartAngle(0);
+        InnerCircle.setLength(360.0f);
+        InnerCircle.setFill(Color.WHITE);
+        InnerCircle.setType(ArcType.ROUND);
+
+        AnchorPane pane = new AnchorPane();
+        pane.getChildren().addAll(ProgressBarHolder, ProgressBarProgress, InnerCircle);
+
+        dashboard_Header.getChildren().addAll(dashboard_Title,pane);
+
 
         // add to dashboard
         getChildren().add(dashboard_Header);
