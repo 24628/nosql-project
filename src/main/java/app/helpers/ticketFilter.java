9804928b18ba16f72440e77bf0c783a9aaa912be
GEZUS95 <ceesgribnau@hotmail.com>
2021-10-12 11:06:46 +0200

    added status filter

--- src/main/java/app/helpers/ticketFilter.java
+++ src/main/java/app/helpers/ticketFilter.java
@@ -6,7 +6,8 @@ import org.bson.conversions.Bson;
 public class ticketFilter {
     public Bson filterTickets(String newValue){
         Bson filter = Filters.or(Filters.regex("incident", ".*" + newValue + ".*", "i")
-                , Filters.regex("description", ".*" + newValue + ".*", "i"));
+                , Filters.regex("description", ".*" + newValue + ".*", "i")
+                , Filters.regex("status", ".*" + newValue + ".*", "i"));
         return filter;
     }
 }
